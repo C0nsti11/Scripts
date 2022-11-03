95dbbdc49c33fa2673c5e3dd5a5c10db1c22c5df
Saurav Nihal Minz <39146953+C0nsti11@users.noreply.github.com>
2022-11-03 12:25:01 +0300

    Update speedtest_install.sh

--- speedtest_install.sh
+++ speedtest_install.sh
@@ -1,5 +1,5 @@
 ## If migrating from prior bintray install instructions please first...
-# sudo rm /etc/apt/sources.list.d/speedtest.list
+# sudo rm /etc/apt/sources.list.d/ookla_speedtest-cli.list
 # sudo apt-get update
 # sudo apt-get remove speedtest
 ## Other non-official binaries will conflict with Speedtest CLI
@@ -9,4 +9,5 @@ sudo apt-get install curl
 ## System like kali whose os is not the list use the second curl command
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 # curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo os=debian dist=bullseye bash
+# curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo os=ubuntu dist=jammy bash
 sudo apt-get install speedtest
